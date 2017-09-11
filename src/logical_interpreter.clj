99d01e24fe27374e9a5de4e608bf991301e74bf5
Nicolás Vazquez <nicolas.vazquez@fdvsolutions.com>
2017-09-10 23:33:10 -0300

    Create functions to deal with facts and rules and to check if rules and facts in the database imply query

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,14 @@
-(ns logical-interpreter)
+(ns logical-interpreter
+  (:require [parser :refer :all]))
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  nil)
+  (def facts (get-facts database))
+  (def rules (get-rules database))
+
+  (let 
+    [rule (search-rule (normalize query) rules)]
+    (if (and (valid-fact? (normalize query)) (all-valid-facts? facts) (not (nil? rules)))
+      (or (and (not (nil? rule)) (apply-rule? rule facts (normalize query))) (is-fact facts (normalize query))))))
---
+++ src/parser.clj
@@ -0,0 +1,48 @@
+(ns parser
+  (:require [clojure.string :as str]))
+
+(defn valid-fact?
+  [fact]
+  (not (= nil (re-matches #"^[a-z0-9]+\([a-z0-9,]+\)$" fact))))
+
+(defn valid-rule?
+  [rule]
+  (not (= nil (re-matches #"^[a-z]+\(([A-Z]+, )*[A-Z]+\) :- (([a-z]+\(([A-Z]+, )*[A-Z]+\)), )*([a-z]+\(([A-Z]+, )*[A-Z]+\))" rule))))
+
+
+(defn all-valid-facts? [facts]
+  (if (every? true? (map valid-fact? facts)) facts))
+
+(defn all-valid-rules? [rules]
+  (if (every? true? (map valid-rule? rules)) rules))
+
+(defn make-rule [rule]
+  [(get (str/split rule #":-") 0)
+    (str/split (str/replace (get (str/split rule #":-") 1) "),"")!") #"!")])
+
+(defn normalize [string]
+  (str/replace string #"(\ |\n|\t)" ""))
+
+(defn is-fact [facts fact]
+  (= (some #(= % fact) facts) true))
+
+(defn get-vars [fact]
+  (subvec (str/split fact #"(\(|,|\))") 1))
+
+(defn get-facts [data]
+  (all-valid-facts? (map normalize (filter #(and (not (str/blank? %)) (not (.contains % ":-"))) (str/split data #"\.")))))
+
+(defn get-rules [data]
+  (map make-rule (map normalize (filter #(and (not (str/blank? %)) (.contains % ":-")) (str/split data #"\.")))))
+
+(defn build-rule [rawParams actualParams actualFact]
+  (map (fn [fact]
+      (reduce (fn [p1 p2]
+          (str/replace-first p1 p2 (nth actualParams (.indexOf rawParams p2)))) fact rawParams)) actualFact))
+
+(defn search-rule [query rules]
+  (first (filter #(.startsWith (nth % 0)
+    (first (str/split query #"\("))) rules)))
+
+(defn apply-rule? [rule facts query]
+  (= (every? #(is-fact facts %) (build-rule (get-vars (nth rule 0)) (get-vars query) (nth rule 1))) true))
